80b7c133f18eb501cde0c2edfba6ca909876ef03
Oleksiy Pastukhov <oleksiy.pastukhov@senseteq.io>
2021-02-28 21:55:30 +0200

    Initial commit

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,29 @@
+const { getDefaultConfig } = require('metro-config')
+
 module.exports = {
   transformer: {
-    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-  },
-};
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false
+      }
+    }),
+    assetPlugins: ['expo-asset/tools/hashAssetFiles']
+  }
+}
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig()
+
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer')
+    },
+    resolver: {
+      assetExts: assetExts.filter((ext) => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg']
+    }
+  }
+})()
